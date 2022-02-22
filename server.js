1c4eb006f0ce87e35fd1380282a28c018789840f
Devon Wigle <devonwigle@gmail.com>
2022-02-22 11:37:57 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+let fs = require("fs");
+let parse = require("csv-parse").parse;
+
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Words';
+
+app.get('/', (request, response) => {
+  response.send('Oh hey Words');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+const records = [];
+fs.createReadStream("data.csv")
+  .pipe(parse({ delimeter: ",", columns: true}))
+  .on("data", (csvrow) => {
+    records.push(csvrow);
+  })
+  .on("end", () => {
+    records.forEach((entry) => {
+      entry.id = parseInt(entry.id)
+      entry.word = entry.word;
+    });
+    app.locals.words = records;
+  });
+
+app.get("/api/v1/words", (request, response) => {
+  const words = app.locals.words;
+
+  response.json({ words });
+});
